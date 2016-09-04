b496deb8bc13e86f756ce953215dc9d692ea1770
Andreas Kull <akullpp@gmail.com>
2016-09-04 16:46:07 +0200

    feat: add code

---
+++ resource.h
@@ -0,0 +1,17 @@
+#define IDD_MAIN    101
+#define IDC_FILE    1001
+#define IDC_SAVE    1002
+#define IDC_RELOAD  1003
+#define IDC_STATIC1 1004
+#define IDC_STATIC2 1005
+#define IDC_STATIC3 1006
+#define IDC_STATIC4 1007
+#define IDC_STATIC5 1008
+#define IDC_STATIC6 1009
+#define IDC_EDIT1   1010
+#define IDC_EDIT2   1011
+#define IDC_EDIT3   1012
+#define IDC_EDIT4   1013
+#define IDC_EDIT5   1014
+#define IDC_EDIT6   1015
+#define IDC_EDIT7   1016
